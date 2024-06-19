e9e83255f15a9ba62f1e7b53f60cdf03cbb569db
Ahmed Said Muftu <ahmedmuftu@Ahmeds-MBP.home>
2024-06-19 15:42:30 +0300

    Added comments to the code

--- horrific_code.py
+++ horrific_code.py
@@ -1,10 +1,12 @@
-
+# Prompt the user to enter their first and last name
 fn = input("Please enter your Firstname: ")
 ln = input("Please enter your Lastname: ")
 
 
+# Create a function to print out the users fullname
 def user():
     print(f"Your fullname is {fn} {ln}")
 
 
+# call the function
 user()
